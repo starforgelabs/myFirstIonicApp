6d0cc8b110e21c97d1e4e73544b75f052469851b
James Knowles <jamesk@starforgelabs.com>
2015-11-03 20:06:12 -0700

    Attempt to touch camera.

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,37 @@
-var module = angular.module('starter.controllers', [])
+var module = angular.module('starter.controllers', []);
 
-module.controller('DashCtrl', function ($ionicPlatform, $scope) {
+module.controller('DashCtrl', function ($ionicPlatform, $cordovaCamera, $scope) {
     $scope.ready = "Not ready";
+
+    $scope.touchHardware = function () {
+        $scope.camera = "Button Pressed...";
+        $ionicPlatform.ready(function () {
+            $scope.camera = "Trying Setup...";
+            var options = {
+                quality: 50,
+                destinationType: Camera.DestinationType.DATA_URL,
+                sourceType: Camera.PictureSourceType.CAMERA,
+                allowEdit: true,
+                encodingType: Camera.EncodingType.JPEG,
+                targetWidth: 100,
+                targetHeight: 100,
+                popoverOptions: CameraPopoverOptions,
+                saveToPhotoAlbum: false,
+                correctOrientation: true
+            };
+
+            $scope.camera = "Calling Camera...";
+            $cordovaCamera.getPicture(options).then(function (imageData) {
+                $scope.camera = "Success";
+                //var image = document.getElementById('myImage');
+                //image.src = "data:image/jpeg;base64," + imageData;
+            }, function (err) {
+                $scope.camera = "Error: " + err;
+                // error
+            });
+        });
+    };
+
     $ionicPlatform.ready(function () {
         $scope.ready = "Ready";
     });
